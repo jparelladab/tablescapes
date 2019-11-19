afce36d139c5c090c83bdddb2579f9cafbbca025
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-19 13:02:16 +0000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   # resources :owned_tablescapes
-  resources :rented_tablescapes, only: [:index, :show] do
+  resources :tablescapes, only: [:index, :show] do
     resources :bookings, only: :create
   end
   resources :bookings, only: [:index, :show]
