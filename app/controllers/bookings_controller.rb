5efc4d03e4c6cf62c515e6d13b64e8b316bcf39d
joan parellada <joan.parellada789@gmail.com>
2019-11-20 15:15:53 +0000

    added new booking confirmation page and change routes a little

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,27 @@
 class BookingsController < ApplicationController
-  def index
+before_action :set_booking, only: [:show]
+
+  def show
   end
+
+  def create
+    @tablescape = Tablescape.find(params[:tablescape_id])
+    @booking = Booking.new(booking_params)
+    @booking.tablescape_id = @tablescape.id
+    @booking.user_id = current_user.id
+    @booking.total_price = (@tablescape.price_per_person * @booking.attendees)
+    @booking.save
+    redirect_to tablescape_booking_path(@tablescape, @booking)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date_from, :date_to, :total_price, :attendees)
+  end
+
 end
