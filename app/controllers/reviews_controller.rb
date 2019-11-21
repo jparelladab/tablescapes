44739fcf9b50acb12c728facfbd3e66e0c476324
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-21 20:22:44 +0000

    Finishing of Bookings index page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,14 +3,33 @@ class ReviewsController < ApplicationController
   #   @booking = Booking.find(params[:booking_id])
   #   @reviews = Review.where(booking_id: @booking)
   # end
-
-  def create
+  def new
     @review = Review.new
   end
 
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking_id = @booking.id
+    @review.save
+    redirect_to bookings_path
+  end
+
   def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to bookings_path
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
   end
 end
