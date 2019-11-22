f83aff59dc96cbc8db18387fbd5a8002f6ec1742
joan parellada <joan.parellada789@gmail.com>
2019-11-22 12:49:06 +0000

    Trying to fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
+import { initMapbox } from '../plugins/init_mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
-import { initMapbox } from '../plugins/init_mapbox';
 
 loadDynamicBannerText();
 
